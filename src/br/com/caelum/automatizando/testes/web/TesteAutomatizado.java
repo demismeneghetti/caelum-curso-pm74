71e02d3fe90d21e1190f263864a890aec3b6d569
Demis Meneghetti <demismeneghetti@msn.com>
2013-06-12 22:52:22 -0300

    Inclusão do Webdriver para Chrome e as respectivas alterações para execução

--- src/br/com/caelum/automatizando/testes/web/TesteAutomatizado.java
+++ src/br/com/caelum/automatizando/testes/web/TesteAutomatizado.java
@@ -1,30 +1,36 @@
 package br.com.caelum.automatizando.testes.web;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
- 
-public class TesteAutomatizado {
- 
-    public static void main(String[] args) {
-        // abre firefox
-        WebDriver driver = new FirefoxDriver();
-         
-        // acessa o site do google
-        driver.get("http://www.google.com.br/");
-         
-        // Insere o texto no campo "q" do Google!
-        WebElement campoDeTexto = driver.findElement(By.name("q"));
-        campoDeTexto.sendKeys("Caelum");
-         
-        // submete o formulário!
-        campoDeTexto.submit();
-        
-        // realiza o print do resultado!
+import org.openqa.selenium.chrome.ChromeDriver;
 
-        // fecha o navegador Firefox
-        // driver.close();
-        // driver.quit();
-    }
+public class TesteAutomatizado {
+
+	public static void main(String[] args) throws InterruptedException {
+		File fileDriver = new File(
+				"/Users/demismeneghetti/git/caelum-curso-pm74/drivers/chromedriver");
+		System.setProperty("webdriver.chrome.driver",
+				fileDriver.getAbsolutePath());
+
+		// abre firefox
+		WebDriver driver = new ChromeDriver();
+
+		// acessa o site do google
+		driver.get("http://www.google.com.br/");
+
+		// digita no campo com nome "q" do google
+		WebElement campoDeTexto = driver.findElement(By.name("q"));
+		campoDeTexto.sendKeys("Demis Meneghetti");
+
+		// submete o form
+		campoDeTexto.submit();
+
+		// Fecha o navegador.
+		//driver.wait(10);
+		//driver.close();
+		//driver.quit();
+	}
 }
